6b27493cb9f9df910e7910efbbec241a322e2ef7
flick-2me <rawatshubham1801@gmail.com>
2026-07-05 22:44:03 +0530

    Logging and Exception

---
+++ src/exception.py
@@ -0,0 +1,37 @@
+import sys
+from logger import logging
+
+# Spelling fix ki aur format theek kiya
+def error_message_detail(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
+    
+    # File ka naam nikala
+    file_name = exc_tb.tb_frame.f_code.co_filename 
+    
+    # FIX: {[0]} ki jagah {0} use kiya
+    error_message = "Error occured in py script name [{0}] line number [{1}] error message : [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    ) 
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        # Custom message banwaya aur save kiya
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+if __name__== "__main__" :
+
+    try :
+        a= 1/0
+    except Exception as e :
+        logging.error("Divide by 0")
+        
+        raise CustomException(e,sys)
+    
+    
+    
